cb57430191ef27089fd9243d87c3c2c1b76f982e
Aryan Sharma <aryansde2204@gmail.com>
2023-06-04 20:55:52 +0530

    Update 6.php

--- 6.php
+++ 6.php
@@ -1,122 +1,29 @@
 <?php
+// Sample array
+$fruits = array(
+    "apple" => "red",
+    "banana" => "yellow",
+    "orange" => "orange",
+    "grape" => "purple"
+);
 
-// Bubble Sort
-function bubbleSort($arr)
-{
-    $n = count($arr);
-    for ($i = 0; $i < $n - 1; $i++) {
-        for ($j = 0; $j < $n - $i - 1; $j++) {
-            if ($arr[$j] > $arr[$j + 1]) {
-                // Swap
-                $temp = $arr[$j];
-                $arr[$j] = $arr[$j + 1];
-                $arr[$j + 1] = $temp;
-            }
-        }
-    }
-    return $arr;
-}
+// rsort: Sorts the values of an array in descending order
+rsort($fruits);
+echo "Sorted using rsort: ";
+print_r($fruits);
 
-// Selection Sort
-function selectionSort($arr)
-{
-    $n = count($arr);
-    for ($i = 0; $i < $n - 1; $i++) {
-        $minIndex = $i;
-        for ($j = $i + 1; $j < $n; $j++) {
-            if ($arr[$j] < $arr[$minIndex]) {
-                $minIndex = $j;
-            }
-        }
-        // Swap
-        $temp = $arr[$i];
-        $arr[$i] = $arr[$minIndex];
-        $arr[$minIndex] = $temp;
-    }
-    return $arr;
-}
+// asort: Sorts an array in ascending order, maintaining key-value associations
+asort($fruits);
+echo "Sorted using asort: ";
+print_r($fruits);
 
-// Insertion Sort
-function insertionSort($arr)
-{
-    $n = count($arr);
-    for ($i = 1; $i < $n; $i++) {
-        $key = $arr[$i];
-        $j = $i - 1;
-        while ($j >= 0 && $arr[$j] > $key) {
-            $arr[$j + 1] = $arr[$j];
-            $j--;
-        }
-        $arr[$j + 1] = $key;
-    }
-    return $arr;
-}
+// ksort: Sorts an array by keys in ascending order, maintaining key-value associations
+ksort($fruits);
+echo "Sorted using ksort: ";
+print_r($fruits);
 
-// Merge Sort
-function mergeSort($arr)
-{
-    $n = count($arr);
-    if ($n <= 1) {
-        return $arr;
-    }
-
-    $mid = (int) ($n / 2);
-    $left = array_slice($arr, 0, $mid);
-    $right = array_slice($arr, $mid);
-
-    $left = mergeSort($left);
-    $right = mergeSort($right);
-
-    return merge($left, $right);
-}
-
-function merge($left, $right)
-{
-    $result = [];
-    $leftLength = count($left);
-    $rightLength = count($right);
-    $i = $j = 0;
-
-    while ($i < $leftLength && $j < $rightLength) {
-        if ($left[$i] <= $right[$j]) {
-            $result[] = $left[$i];
-            $i++;
-        } else {
-            $result[] = $right[$j];
-            $j++;
-        }
-    }
-
-    while ($i < $leftLength) {
-        $result[] = $left[$i];
-        $i++;
-    }
-
-    while ($j < $rightLength) {
-        $result[] = $right[$j];
-        $j++;
-    }
-
-    return $result;
-}
-
-// Example usage
-$arr = [9, 5, 1, 8, 3, 7, 2, 6, 4];
-echo "Original array: " . implode(", ", $arr) . "\n\n";
-
-// Bubble Sort
-$sortedArr = bubbleSort($arr);
-echo "Bubble Sort: " . implode(", ", $sortedArr) . "\n";
-
-// Selection Sort
-$sortedArr = selectionSort($arr);
-echo "Selection Sort: " . implode(", ", $sortedArr) . "\n";
-
-// Insertion Sort
-$sortedArr = insertionSort($arr);
-echo "Insertion Sort: " . implode(", ", $sortedArr) . "\n";
-
-// Merge Sort
-$sortedArr = mergeSort($arr);
-echo "Merge Sort: " . implode(", ", $sortedArr) . "\n";
+// arsort: Sorts an array in descending order, maintaining key-value associations
+arsort($fruits);
+echo "Sorted using arsort: ";
+print_r($fruits);
 ?>
