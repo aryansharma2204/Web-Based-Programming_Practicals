9690064970a0329a352b73b4c6f7bbbeb1cb3256
Aryan Sharma <aryansde2204@gmail.com>
2023-06-10 10:21:45 +0530

    Create 26.php

---
+++ 26.php
@@ -0,0 +1,34 @@
+<?php
+$servername = "localhost";
+$username = "your_username";
+$password = "your_password";
+$dbname = "your_database_name";
+$tableName = "users"; // Replace with your table name
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    // Fetch all records from the table
+    $selectQuery = "SELECT * FROM $tableName";
+    $stmt = $conn->prepare($selectQuery);
+    $stmt->execute();
+    $records = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    // Display the records in an HTML table
+    echo "<table>";
+    echo "<tr><th>ID</th><th>Name</th><th>Email</th></tr>";
+    foreach ($records as $record) {
+        echo "<tr>";
+        echo "<td>" . $record['id'] . "</td>";
+        echo "<td>" . $record['name'] . "</td>";
+        echo "<td>" . $record['email'] . "</td>";
+        echo "</tr>";
+    }
+    echo "</table>";
+} catch (PDOException $e) {
+    echo "Error: " . $e->getMessage();
+}
+
+$conn = null;
+?>
