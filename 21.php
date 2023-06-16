01615849fca54659d9bc5f129ec843157252e27d
Aryan Sharma <aryansde2204@gmail.com>
2023-06-16 22:19:35 +0530

    Update 21.php

--- 21.php
+++ 21.php
@@ -5,49 +5,54 @@
 </head>
 <body>
     <h1>File Upload and Download</h1>
-    
-    <?php
-    // Check if a file has been uploaded
-    if(isset($_FILES['fileToUpload'])){
-        $file = $_FILES['fileToUpload'];
-        
-        // File details
-        $fileName = $file['name'];
-        $fileTmpName = $file['tmp_name'];
-        $fileSize = $file['size'];
-        $fileError = $file['error'];
-        $fileType = $file['type'];
-        
-        // Specify the directory where the file should be uploaded
-        $uploadDir = 'uploads/';
-        
-        // Check if there are no errors
-        if($fileError === 0){
-            // Move the uploaded file to the desired directory
-            $fileDestination = $uploadDir . $fileName;
-            move_uploaded_file($fileTmpName, $fileDestination);
-            echo "File uploaded successfully!";
-        } else {
-            echo "Error uploading the file.";
-        }
-    }
-    ?>
-    
+
+    <!-- File Upload Form -->
     <form action="" method="POST" enctype="multipart/form-data">
-        <input type="file" name="fileToUpload">
-        <button type="submit" name="submit">Upload</button>
+        <input type="file" name="file" required>
+        <input type="submit" name="upload" value="Upload">
     </form>
-    
-    <h2>Uploaded Files</h2>
-    
+
     <?php
-    // List all the files in the upload directory
-    $files = scandir($uploadDir);
-    foreach($files as $file){
-        if($file !== '.' && $file !== '..'){
-            echo '<a href="'.$uploadDir.$file.'">'.$file.'</a><br>';
+    // File Upload Logic
+    if (isset($_POST['upload'])) {
+        $targetDir = "uploads/";
+        $fileName = basename($_FILES["file"]["name"]);
+        $targetFilePath = $targetDir . $fileName;
+        $fileType = pathinfo($targetFilePath, PATHINFO_EXTENSION);
+
+        // Check if file already exists
+        if (file_exists($targetFilePath)) {
+            echo "File already exists.";
+        } else {
+            // Check if the file is a valid upload
+            if (is_uploaded_file($_FILES["file"]["tmp_name"])) {
+                // Move the uploaded file to the specified directory
+                if (move_uploaded_file($_FILES["file"]["tmp_name"], $targetFilePath)) {
+                    echo "File uploaded successfully.";
+                } else {
+                    echo "Error uploading the file.";
+                }
+            } else {
+                echo "Invalid file upload.";
+            }
         }
     }
     ?>
+
+    <hr>
+
+    <!-- File Download Links -->
+    <h2>Uploaded Files</h2>
+    <ul>
+        <?php
+        // Get the list of uploaded files
+        $files = glob("uploads/*");
+
+        foreach ($files as $file) {
+            $fileName = basename($file);
+            echo "<li><a href='download.php?file=$file'>$fileName</a></li>";
+        }
+        ?>
+    </ul>
 </body>
 </html>
